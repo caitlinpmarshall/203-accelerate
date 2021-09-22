dad2932e84051b475f8b359f7da0a7ada863555b
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-09-22 15:16:09 -0400

    Adds single post template for careers page

---
+++ wp-content/themes/accelerate-theme-child/single-careers.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The Template for displaying a single job posting
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+<section class="single-page">
+	<div class="site-content">
+		<div class="main-content">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part('content', get_post_format()); ?>
+				<div class="post-social-icons">
+					<?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
+				</div>
+				<?php comments_template(); ?>
+			<?php endwhile; ?>
+
+		</div>
+	</div>
+</section>
+
+<nav id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr; <span>Back to posts</span></a></div>
+</nav>
+
+<?php get_footer(); ?>
