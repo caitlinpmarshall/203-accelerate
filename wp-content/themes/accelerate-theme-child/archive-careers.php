71d9ae71f0f2c7c5d27ea3c7cda604509e11dabe
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-09-29 13:41:30 -0400

    Begins careers archive page

---
+++ wp-content/themes/accelerate-theme-child/archive-careers.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the careers archive page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$job_title = get_field('job_title');
+				$location = get_field('location');
+				$job_type = get_field('job_type');
+            ?>
+                <article class="case-study-archive">
+					<aside class="case-study-sidebar-archive">
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<h4><span><?php echo $job_title; ?></span></h4>
+                        <h4><span><?php echo $location; ?></span></h4>
+						<h4><span><?php echo $job_type; ?></span></h4>
+					</aside>
+                </article>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
